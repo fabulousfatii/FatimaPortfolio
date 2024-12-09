76b5ead30a74a3bbf5d7e4d584f3c169b30958e4
fatima <fatimasaleem778899@gmail.com>
2024-12-09 20:55:15 +0500

    change cards

--- src/pages/Projectspage.jsx
+++ src/pages/Projectspage.jsx
@@ -16,6 +16,13 @@ function Projectspage() {
                title={'Blog Website'}
                description={"A responsive blog website built with react js. Firbase for storage"}
                image={"https://images.unsplash.com/photo-1619608135352-868e8313e121?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+
+<div className=' w-96 max-md:w-72  max-sm:w-60  h-80 z-40 rounded-xl flex flex-col justify-start p-6  border relative overflow-hidden hover:scale-105 transition-all duration-200 cursor-pointer '>
+            <div className={`shadow3 top-0 left-0 absolute w-96 h-80 -z-2 bg-[url("https://images.unsplash.com/photo-1619608135352-868e8313e121?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-cover bg-opacity-40  backdrop-filter backdrop-blur-xl rounded-xl flex flex-col  items-center p-  border hover:blur-sm `} ></div>
+             <h1 className='text-sm z-40 text-yellow-500 mb-2 '>latest</h1>
+             <h1 className='text-3xl z-40 font-bold '>Blog Website</h1>
+             <p className='text-xl z-40 text-gray-400 '> A responsive blog website built with react js. Firbase for storage</p>
+            </div>
   
   
                <Projectcard
@@ -23,10 +30,19 @@ function Projectspage() {
                description={"A basic e-commerce responsive website focusing on functionality"}
                image={"https://img.freepik.com/premium-photo/online-shop-dark-blue-with-shopping-element-3d-illustration_925613-52327.jpg"}/>
   
-              <Projectcard
-               title={'Employee Management'}
-               description={" Allowing admin to give task to employess , have admin and employee dashboard"}
-               image={"https://images.unsplash.com/photo-1530374260450-4bd19102f63d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+  <div className=' w-96 max-md:w-72  max-sm:w-60  h-80 z-40 rounded-xl flex flex-col justify-start p-6  border relative overflow-hidden hover:scale-105 transition-all duration-200 cursor-pointer '>
+            <div className={`shadow3 top-0 left-0 absolute w-96 h-80 -z-2 bg-[url("https://img.freepik.com/premium-photo/online-shop-dark-blue-with-shopping-element-3d-illustration_925613-52327.jpg")] bg-cover bg-opacity-40  backdrop-filter backdrop-blur-xl rounded-xl flex flex-col  items-center p-  border hover:blur-sm `} ></div>
+             <h1 className='text-sm z-40 text-yellow-500 mb-2 '>latest</h1>
+             <h1 className='text-3xl z-40 font-bold '>E-commerce website</h1>
+             <p className='text-xl z-40 text-gray-400 '>A basic e-commerce responsive website focusing on functionality</p>
+            </div>
+              
+               <div className=' w-96 max-md:w-72  max-sm:w-60  h-80 z-40 rounded-xl flex flex-col justify-start p-6  border relative overflow-hidden hover:scale-105 transition-all duration-200 cursor-pointer '>
+            <div className={`shadow3 top-0 left-0 absolute w-96 h-80 -z-2 bg-[url("https://images.unsplash.com/photo-1530374260450-4bd19102f63d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-cover bg-opacity-40  backdrop-filter backdrop-blur-xl rounded-xl flex flex-col  items-center p-  border hover:blur-sm `} ></div>
+             <h1 className='text-sm z-40 text-yellow-500 mb-2 '>latest</h1>
+             <h1 className='text-3xl z-40 font-bold '>Employee Management</h1>
+             <p className='text-xl z-40 text-gray-400 '>Allowing admin to give task to employess , have admin and employee dashboard </p>
+            </div>
   
             </div>
   
